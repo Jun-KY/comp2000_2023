4c36b43bfe45dae436888cc873edd440e32b3be2
Youngjun Kwon <Youngjun.Kwon@students.mq.edu.au>
2023-08-23 15:55:16 +1000

    Week 5 prep

--- src/Main.java
+++ src/Main.java
@@ -6,35 +6,36 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Main extends JFrame {
-    public static void main(String[] args) throws Exception {
-      Main window = new Main();
-      window.run();
+  public static void main(String[] args) throws Exception {
+    Main window = new Main();
+    window.run();
+  }
+
+  class Canvas extends JPanel {
+    Stage stage;
+
+    public Canvas() throws IOException {
+      setPreferredSize(new Dimension(720, 720));
+      stage = StageReader.readStage("data/stage1.rvb");
     }
 
-    class Canvas extends JPanel {
-      Stage stage;
-      public Canvas() {
-        setPreferredSize(new Dimension(720, 720));
-        stage = StageReader.readStage("data/stage11.rvb");
-      }
-
-      @Override
-      public void paint(Graphics g) {
-        stage.paint(g, getMousePosition());
-      }
+    @Override
+    public void paint(Graphics g) {
+      stage.paint(g, getMousePosition());
     }
+  }
 
-    private Main() {
-      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      Canvas canvas = new Canvas();
-      this.setContentPane(canvas);
-      this.pack();
-      this.setVisible(true);
-    }
+  private Main() throws IOException {
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    Canvas canvas = new Canvas();
+    this.setContentPane(canvas);
+    this.pack();
+    this.setVisible(true);
+  }
 
-    public void run() {
-      while(true) {
-        repaint();
-      }
+  public void run() {
+    while (true) {
+      repaint();
     }
+  }
 }
